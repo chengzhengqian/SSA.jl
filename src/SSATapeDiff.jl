1a68f8fdcd3c36fed82cbee619cfa8b5ca214054
chengzhengqian-game <chengzhengqian@gmail.com>
2021-11-23 02:56:34 -0500

    update Diff

--- src/SSATapeDiff.jl
+++ src/SSATapeDiff.jl
@@ -1,7 +1,9 @@
 # add forward for SSATape
-export Derivative, evalDiff, initDefaultDiffDict, addToOutput, defaultDerivatives
+export Derivative, evalDiff, initDefaultDiffDict, addToOutput, defaultDerivatives, diffTape
 # we first define a structure to represent derivatives
 # right now, we only consider first order
+# we now use the customize derivative formulas instead of using SymEngine
+#TODO add more conveinent API
 struct Derivative
     target::Node
     arg::Node
@@ -11,21 +13,104 @@ function Base.show(io::IO,diff::Derivative)
     print(io,"∂$(diff.target)/∂$(diff.arg)")
 end
 
-function evalDiff(ssaform::SSAForm,arg::Node,dict::Dict{Derivative,Node},ssatape::SSATape)
-    expr=Basic("$(ssaform)")
-    result=Basic(0)
-    for v in free_symbols(expr)
-        v_=evalTape(Symbol("$(v)"),ssatape)
-        result+=diff(expr,v)*convert(Basic,evalDiff(v_,arg,dict,ssatape),ssatape)
+"""
+we assume ssaform only have * and -, which is true from the new way to construct the ssatape
+"""
+function evalDiff(ssaform::SSAForm,arg::Node,diffMap::Dict{Derivative,Node},ssatape::SSATape)
+    if(ssaform.op==(:*))
+        return evalDiffMul(ssaform.args,arg,diffMap,ssatape)
+    elseif (ssaform.op==(:+))
+        return evalDiffAdd(ssaform.args,arg,diffMap,ssatape)
+    else
+        error("unsupported op for $(ssaform)")
+    end    
+end
+
+# old version
+# function evalDiff(ssaform::SSAForm,arg::Node,dict::Dict{Derivative,Node},ssatape::SSATape)
+#     expr=Basic("$(ssaform)")
+#     result=Basic(0)
+#     for v in free_symbols(expr)
+#         v_=evalTape(Symbol("$(v)"),ssatape)
+#         result+=diff(expr,v)*convert(Basic,evalDiff(v_,arg,dict,ssatape),ssatape)
+#     end
+#     evalTape(result,ssatape)
+# end
+
+"""
+ssafrom=(:+,terms)
+
+"""
+function evalDiffAdd(terms::Array{Node,1}, arg::Node, diffMap::Dict{Derivative,Node},ssatape::SSATape)
+    ∂terms∂arg=Array{Node,1}()
+    for term in terms
+        ∂term∂arg=evalDiff(term,arg,diffMap,ssatape)
+        if(∂term∂arg!=ssatape(0.0))
+            push!(∂terms∂arg,∂term∂arg)
+        end        
     end
-    evalTape(result,ssatape)
+    return sumTerms(∂terms∂arg,ssatape)
+end
+
+"""
+sum nodes. Assume all of them is non-zero
+"""
+function sumTerms(terms::Array{Node,1},ssatape::SSATape)
+    n_terms=length(terms)
+    if(n_terms==0)
+        return ssatape(0.0)
+    elseif(n_terms==1)
+        return terms[1]
+    else
+        return ssatape(SSAForm(:+, terms))
+    end    
+end
+"""
+assuming all of term is non-zero and not identity
+"""
+function mulTerms(terms::Array{Node,1},ssatape::SSATape)
+    n_terms=length(terms)
+    if(n_terms==0)
+        return ssatape(1.0)
+    elseif(n_terms==1)
+        return terms[1]
+    else
+        return ssatape(SSAForm(:*, terms))
+    end    
+end
+
+
+"""
+ssaform=(:*, terms)
+idx=1
+check wether the the derivative is zero or one to simplify the calculation
+"""
+function evalDiffMul(terms::Array{Node,1}, arg::Node, diffMap::Dict{Derivative,Node},ssatape::SSATape)
+    ∂terms∂arg=Array{Node,1}()
+    for idx in 1:length(terms)
+        term=terms[idx]
+        ∂term∂arg=evalDiff(term,arg,diffMap,ssatape)
+        if(∂term∂arg!=ssatape(0.0))
+            terms_new=copy(terms)
+            if(∂term∂arg!=ssatape(1.0))
+                terms_new[idx]=∂term∂arg
+            else
+                deleteat!(terms_new,idx)
+            end
+            push!(∂terms∂arg,mulTerms(terms_new,ssatape))
+        end        
+    end
+    return sumTerms(∂terms∂arg,ssatape)
 end
 
 
 
+"""
+evaluate the node to node derivative
+"""
 function evalDiff(node::Node,arg::Node,dict::Dict{Derivative,Node},ssatape::SSATape)
     if(node isa NumberNode)
-        return evalTape(Basic(0),ssatape)
+        return evalTape(0,ssatape)
     end    
     ∂node∂arg=Derivative(node,arg)
     if(node isa ComputeNode)
@@ -46,19 +131,20 @@ end
 add print information
 """
 function evalDiff(args::Array{T,1},dict::Dict{Derivative,Node} ,ssatape::SSATape) where T<:Node
-    current_compute_size=length(ssatape.compute)
-    idx=0
-    # we use 1000 steps to show and perform gc
-    N_print_and_gc_idx=round(Int,current_compute_size/1000)
-    for i in 1:current_compute_size
-        # print and perform gc
-        if(idx>N_print_and_gc_idx)
-            idx=0
-            print("$(i)/$(current_compute_size)\n")
-            customized_gc()
-        end
-        idx+=1
-        evalDiff(ComputeNode(i-1),args,dict,ssatape)
+    evalDiff(length(ssatape.compute)-1,args,dict,ssatape)
+end
+
+"""
+idx start from 0
+idx=2000
+"""
+function evalDiff(idx::Int,args::Array{T,1},dict::Dict{Derivative,Node} ,ssatape::SSATape) where T<:Node
+    N_print_idx=trunc(Int,idx/100)    
+    for i in 0:idx        
+        evalDiff(ComputeNode(i),args,dict,ssatape)
+        if(i%N_print_idx==0)
+            print("evalDiff $(i)/$(idx)\n")
+        end        
     end    
 end
 
@@ -76,8 +162,12 @@ function initDefaultDiffDict(args::Array{T,1},ssatape::SSATape) where T<:Node
     end
     dict
 end
-
-
+"""
+assuming we need the diff for all inputs
+"""
+function initDefaultDiffDict(ssatape::SSATape)
+    initDefaultDiffDict(ssatape.(getInputArgs(ssatape)),ssatape)
+end
 # now, we need to add the derivative to the output
 # to facilatate the further use, we use a string ∂y∂x
 """
@@ -116,4 +206,27 @@ The default order to store the first order derivatives
 """
 function defaultDerivatives(targets::Array{Symbol,1},args::Array{Symbol,1})
     [Symbol("∂$(target_)∂$(arg_)")  for target_ in targets for arg_ in args]
+end
+
+
+"""
+we provide the convinient API by ensure that we automatically compute all the necessary forward steps
+diffMap=initDefaultDiffDict(ssatape.(getInputArgs(ssatape)),ssatape)
+diffMap=initDefaultDiffDict(ssatape)
+typeof(diffMap).
+So one only need diffTape and a diffMap (for efficiency to reuse the derivatives)
+"""
+function diffTape(targets::Array{Symbol,1},args::Array{Symbol,1},diffMap::Dict{Derivative,Node} ,ssatape::SSATape)
+    max_compute_node_idx=0
+    for target in targets
+        node=ssatape.output[target]
+        if(node isa ComputeNode)
+            if(node.idx>max_compute_node_idx)
+                max_compute_node_idx=node.idx
+            end            
+        end        
+    end
+    args_node=ssatape.(args)
+    evalDiff(max_compute_node_idx,args_node,diffMap,ssatape)
+    addToOutput(targets,args,diffMap,ssatape)
 end
--- src/test_script.jl
+++ src/test_script.jl
@@ -26,6 +26,8 @@ using MathExpr
 engine=ExprEngine()
 x,y,z=engine.([:x,:y,:z])
 expr=x+y*2+z*x
+r=ssatape(expr)
+ssatape(r,:r)
 ssatape(expr,term_map)
 ssatape(:+,[ssatape(1.0),ssatape(2.0)])
 ssatape.compute
@@ -42,3 +44,28 @@ r=ssatape(expr)
 ssatape(r,:r)
 func_2=SSACompiledFunc(ssatape,[:r])
 func_2(test_input...)
+
+# we now test the diff, we try not use Basic in the new implementation
+diffMap=initDefaultDiffDict(ssatape.(getInputArgs(ssatape)),ssatape)
+
+ssaform=getfromA(ssatape.compute,ComputeNode(0))
+arg=InputNode(0)
+import SSA.evalDiff
+import SSA.initDefaultDiffDict
+ssatape.number
+ssatape.compute
+evalDiff(ssaform,arg,diffMap,ssatape)
+evalDiff(InputNode(0),InputNode(0),diffMap,ssatape)
+ssaform.op
+terms=ssaform.args
+evalDiffMul(terms,arg,diffMap,ssatape)
+ssatape.compute
+
+evalDiff(ssatape.(getInputArgs(ssatape)),diffMap,ssatape)
+
+addToOutput([:r],[:x,:y,:z],diffMap,ssatape)
+output_diff,output_syms=diffTape([:r],[:x,:y,:z],diffMap,ssatape)
+
+targets=[:r]
+target=:r
+args=[:x,:y,:z]
